3768596d35d544c03f0f51419d63f9fc804dfb82
Joares Miranda <joares.miranda@gmail.com>
2022-03-12 13:13:32 -0300

    rename variables and add call to pagination

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,10 @@
 const cardsGroup = document.querySelector('.cards-group')
 
+const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+});
+
 const requestURL = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1'
 const request = new XMLHttpRequest();
 
@@ -8,36 +13,33 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-    const productList = request.response;
-    cards(productList);
+    const arrayProducts = request.response;
+    product(arrayProducts.products);
+    pagination(arrayProducts.nextPage);
 }
 
 
-const cards = (item) => {
-
-    const formatter = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-    });
-
-    const items = item.products.map(product => {
+const product = (productData) => {
+    const items = productData.map(data => {
         const card = `
             <div class="card">
                 <div class="card-header">
-                    <img src="http:${product.image}">
+                    <img src="http:${data.image}">
                 </div>
                 <div class="card-content">
-                    <h3 class="card-content__title">${product.name}</h3>
-                    <p class="card-content__description">${product.description}</p>
-                    <p class="card-content__prices__oldprice">De: ${formatter.format(product.oldPrice)}</p>
-                    <p class="card-content__prices__price">Por: ${formatter.format(product.price)}</p>
-                    <p class="card-content__prices__installments">ou ${product.installments.count}x de ${formatter.format(product.installments.value)}</p>
+                    <h3 class="card-content__title">${data.name}</h3>
+                    <p class="card-content__description">${data.description}</p>
+                    <p class="card-content__prices__oldprice">De: ${formatter.format(data.oldPrice)}</p>
+                    <p class="card-content__prices__price">Por: ${formatter.format(data.price)}</p>
+                    <p class="card-content__prices__installments">ou ${data.installments.count}x de ${formatter.format(data.installments.value)}</p>
                     <p class="card-content__button"><a href="#" class="button button-card" value="Comprar">Comprar</a></p>
                 </div>
             </div>
         `
+        return cardsGroup.innerHTML += card;
+    })    
+}
 
-        return cardsGroup.innerHTML += card       
-    });
-
+const pagination = (currentPage) => {
+    console.log(currentPage)
 }

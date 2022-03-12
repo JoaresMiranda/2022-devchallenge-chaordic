deac724b2b9cc2bd5c88522e4fe6d3ef2b875cf1
Joares Miranda <joares.miranda@gmail.com>
2022-03-11 22:36:57 -0300

    create dynamic card for products

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,36 +3,37 @@ const cardsGroup = document.querySelector('.cards-group')
 const requestURL = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1'
 const request = new XMLHttpRequest();
 
-request.open('REST', request);
+request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
 request.onload = function() {
-    var product = request.response;
-    card(product);
+    const productList = request.response;
+    console.log(productList.products[0].installments.count)
+    cards(productList);
 }
 
-const card = (jsonObj) => {
-    const image = jsonObj['image'];
-    const title = jsonObj['name'];
-    const description = jsonObj['description'];
-    const oldPrice = jsonObj['oldPrice'];
-    const price = jsonObj['price'];
+const cards = (item) => {
+    const items = item.products.map(product => {
+        const card = `
+            <div class="card">
+                <div class="card-header">
+                    <img src="http:${product.image}">
+                </div>
+                <div class="card-content">
+                    <h3 class="card-content__title">${product.name}</h3>
+                    <p class="card-content__description">${product.description}</p>
+                    <p class="card-content__prices__oldprice">De: R$ ${product.oldPrice}</p>
+                    <p class="card-content__prices__price">Por: R$ ${product.price}</p>
+                    <p class="card-content__prices__installments">ou ${product.installments.count}x de R$ ${product.installments.value}</p>
+                    <p class="card-content__button"><a href="#" class="button button-card" value="Comprar">Comprar</a></p>
+                </div>
+            </div>
+        `
 
-    cardsGroup.innerHTML = `
-    <div class="card">
-        <div class="card-header">
-            <img src="${image}">
-        </div>
-        <div class="card-content">
-            <h3 class="card-content__title">${title}</h3>
-            <p class="card-content__description">${description}</p>
-            <p class="card-content__prices__oldprice">De: R$ ${oldPrice}</p>
-            <p class="card-content__prices__price">Por: R$ ${price}</p>
-            <p class="card-content__prices__installments">ou 2x de R$9,99</p>
-            <p class="card-content__button"><a href="#" class="button button-card" value="Comprar">Comprar</a></p>
-        </div>
-    </div>
-    `
+        return cardsGroup.innerHTML += card       
+    });
+
+    return console.log(items[0])
+
 }
-

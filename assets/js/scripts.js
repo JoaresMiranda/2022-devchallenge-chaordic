1757b70719b75c554529723f871f928d51a16f0d
Joares Miranda <joares.miranda@gmail.com>
2022-03-25 21:08:08 -0300

    Create new functions to build the cards

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,14 @@ const formatter = new Intl.NumberFormat('pt-BR', {
     currency: 'BRL',
 });
 
-const requestURL = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1'
+
+const currentPage = (pageNumber) => {
+    return pageNumber = 1;
+}
+
+const urlAPI = `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${currentPage()}`
+
+const requestURL = urlAPI;
 const request = new XMLHttpRequest();
 
 request.open('GET', requestURL);
@@ -18,28 +25,34 @@ request.onload = function() {
     pagination(arrayProducts.nextPage);
 }
 
-
-const product = (productData) => {
-    const items = productData.map(data => {
-        const card = `
-            <div class="card">
-                <div class="card-header">
-                    <img src="http:${data.image}">
-                </div>
-                <div class="card-content">
-                    <h3 class="card-content__title">${data.name}</h3>
-                    <p class="card-content__description">${data.description}</p>
-                    <p class="card-content__prices__oldprice">De: ${formatter.format(data.oldPrice)}</p>
-                    <p class="card-content__prices__price">Por: ${formatter.format(data.price)}</p>
-                    <p class="card-content__prices__installments">ou ${data.installments.count}x de ${formatter.format(data.installments.value)}</p>
-                    <p class="card-content__button"><a href="#" class="button button-card" value="Comprar">Comprar</a></p>
-                </div>
-            </div>
-        `
-        return cardsGroup.innerHTML += card;
-    })    
+const buildCard = (data) => {
+    return `
+    <div class="card">
+        <div class="card-header">
+            <img src="http:${data.image}">
+        </div>
+        <div class="card-content">
+            <h3 class="card-content__title">${data.name}</h3>
+            <p class="card-content__description">${data.description}</p>
+            <p class="card-content__prices__oldprice">De: ${formatter.format(data.oldPrice)}</p>
+            <p class="card-content__prices__price">Por: ${formatter.format(data.price)}</p>
+            <p class="card-content__prices__installments">ou ${data.installments.count}x de ${formatter.format(data.installments.value)}</p>
+            <p class="card-content__button"><a href="#" class="button button-card" value="Comprar">Comprar</a></p>
+        </div>
+    </div>
+    `
 }
 
-const pagination = (currentPage) => {
-    console.log(currentPage)
+
+const product = (listProducts) => {
+    const items = listProducts.map( products => {
+        return buildCard(products)
+    })
+
+    return cardsGroup.innerHTML = items
+}
+
+
+const pagination = (nextPage) => {
+    console.log(nextPage)
 }
